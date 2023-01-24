169bdc06bac55a528a93cfd4ac562f83556dfc09
Oliver Rasoli <122869683+oliverRasoli@users.noreply.github.com>
2023-01-24 09:38:40 +0100

    feat: profile entity init

---
+++ ORM/src/entity/auth/Profiles.ts
@@ -0,0 +1,27 @@
+import { RandomUUIDOptions } from 'crypto';
+import { Entity, Column, PrimaryGeneratedColumn, Timestamp } from 'typeorm';
+
+/**
+ * This entity contains the profile information for a user that is not login related.
+ * All login related information is stored in the supabase builtin table 'auth.users'
+ * which this entity is related to. The constraint to that table cannot be made through typeorm
+ * since it is in another schema but it is created through an sql script (SQL/constraints/profile-constraint.sql)
+ * which is executed after running the 'npm run sync' command
+ */
+@Entity()
+export class Profiles {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  age: number;
+
+  @Column()
+  created_at: Timestamp;
+}
--- ORM/src/populate/scripts/test-users.ts
+++ ORM/src/populate/scripts/test-users.ts
@@ -1,6 +1,6 @@
 import { EntityManager } from 'typeorm';
-import { Role } from '../../entity/auth/Role';
-import { Profile } from '../../entity/auth/Profile';
+
+import { Profiles } from '../../entity/auth/Profiles';
 import { createOrUseSupabaseUser } from '../util/user-utilities';
 import { PopulateScriptExecutor } from '../util/types';
 
@@ -8,23 +8,16 @@ export const execute: PopulateScriptExecutor = async (manager: EntityManager) =>
   const authUser1 = await createOrUseSupabaseUser('test1@mail.dk', '12345678');
   const authUser2 = await createOrUseSupabaseUser('test2@mail.dk', '12345678');
 
-  const adminRole = await manager.findOne(Role, { where: { name: 'admin' } });
-  const userRole = await manager.findOne(Role, { where: { name: 'user' } });
-
-  const adminProfile = new Profile();
-  adminProfile.first_name = 'Bob';
-  adminProfile.last_name = 'Dylan';
-  adminProfile.age = 20;
-  adminProfile.roles = [adminRole];
+  const adminProfile = new Profiles();
+  adminProfile.username = 'Bob';
+  adminProfile.password = 'Bobby';
   adminProfile.id = authUser1.id;
 
   await manager.save(adminProfile);
 
-  const profile = new Profile();
-  profile.first_name = 'Bob';
-  profile.last_name = 'Marley';
-  profile.age = 24;
-  profile.roles = [userRole];
+  const profile = new Profiles();
+  profile.username = 'Bob';
+  profile.password = 'Marley';
   profile.id = authUser2.id;
 
   await manager.save(profile);
