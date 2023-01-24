0e61030a05f22fdaba7433d5cc2fe072e8dc982f
Oliver Rasoli <122869683+oliverRasoli@users.noreply.github.com>
2023-01-24 10:12:22 +0100

    feat: added dogs to the test

--- ORM/src/populate/scripts/test-users.ts
+++ ORM/src/populate/scripts/test-users.ts
@@ -3,6 +3,7 @@ import { EntityManager } from 'typeorm';
 import { Profiles } from '../../entity/auth/Profiles';
 import { createOrUseSupabaseUser } from '../util/user-utilities';
 import { PopulateScriptExecutor } from '../util/types';
+import { Dogs } from '../../entity/Items/Dogs';
 
 export const execute: PopulateScriptExecutor = async (manager: EntityManager) => {
   const authUser1 = await createOrUseSupabaseUser('test1@mail.dk', '12345678');
@@ -23,4 +24,25 @@ export const execute: PopulateScriptExecutor = async (manager: EntityManager) =>
   profile.created_at = authUser2.created_at;
 
   await manager.save(profile);
+
+  const dog1 = new Dogs();
+  dog1.race_name = 'French Bulldog';
+  dog1.description = 'Short and stout';
+  dog1.uuid = profile;
+
+  await manager.save(dog1);
+
+  const dog2 = new Dogs();
+  dog2.race_name = 'Corgi';
+  dog2.description = 'Short and long';
+  dog2.uuid = profile;
+
+  await manager.save(dog2);
+
+  const dog3 = new Dogs();
+  dog3.race_name = 'Borzoi';
+  dog3.description = 'Hairy and tall';
+  dog3.uuid = adminProfile;
+
+  await manager.save(dog3);
 };
