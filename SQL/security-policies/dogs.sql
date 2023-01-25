0c6417402fdf663b96ca3aeb3baa4b993891aee4
Oliver Rasoli <122869683+oliverRasoli@users.noreply.github.com>
2023-01-25 14:01:32 +0100

    feat: dog insert policy

--- SQL/security-policies/dogs.sql
+++ SQL/security-policies/dogs.sql
@@ -7,3 +7,10 @@ FOR SELECT USING (
   auth.uid() = uuid
 );
 
+DROP POLICY IF EXISTS insert_dog_data on public.dogs;
+CREATE POLICY insert_dog_data
+ON public.dogs
+FOR INSERT 
+TO authenticated
+WITH CHECK (true);
+
